cdcf93bb996d95d3fbedacc90192e75532ec4b92
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-07-16 17:32:11 +0200

    removed minified bundle. match only start of the word

--- vendor/assets/javascripts/twitter/typeahead/typeahead.bundle.js
+++ vendor/assets/javascripts/twitter/typeahead/typeahead.bundle.js
@@ -950,7 +950,7 @@
             for (var i = 0, len = patterns.length; i < len; i++) {
                 escapedPatterns.push(_.escapeRegExChars(patterns[i]));
             }
-            regexStr = wordsOnly ? "\\b(" + escapedPatterns.join("|") + ")\\b" : "(" + escapedPatterns.join("|") + ")";
+            regexStr = wordsOnly ? "\\b(" + escapedPatterns.join("|") + ")\\b" : "^(" + escapedPatterns.join("|") + ")| (" + escapedPatterns.join("|") + ")";
             return caseSensitive ? new RegExp(regexStr) : new RegExp(regexStr, "i");
         }
     }(window.document);
